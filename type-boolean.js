bfcfe45f0558c1534284baaf439cdf331ec03f6c
Vinicius9821 <vinicius-atilio@outlook.com>
2021-12-27 19:59:43 -0800

    exemplos da aula 01

---
+++ type-boolean.js
@@ -0,0 +1,11 @@
+// boolean = true and false
+
+const primeiroNumero = 10*5;
+const segundoNumero = 10;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto = "Alura"
+const texto2 = "Alura";
+
+console.log(texto === texto2)
---
+++ type-string.js
@@ -0,0 +1,14 @@
+const texto1 = "Hello World!";
+const texto2 = 'Hello, World!';
+const senha = 'senhaSuperSegura456!';
+const StringDeNumeros = "34567";
+
+const citacao = 'Meu nome e ';
+const meuNome = "Bingulin";
+
+// concatenacao (+)
+
+console.log(citacao + meuNome)
+
+//template sring ou template literal
+
